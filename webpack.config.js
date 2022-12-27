8d205312bd5cb3a9a30e4980ab9ba23c763082b8
marefpceo <marefpceo@gmail.com>
2022-12-26 19:44:33 -0800

    Started CSS, added font, updated webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -3,20 +3,21 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  devtool: 'inline-source-map',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
-      {
-        test: /\.css$/i,
-        use: ['css-loader', 'style-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
+      // {
+      //   test: /\.css$/i,
+      //   use: ['css-loader', 'style-loader'],
+      // },
+      // {
+      //   test: /\.(png|svg|jpg|jpeg|gif)$/i,
+      //   type: 'asset/resource',
+      // },
     ],
   },
 };
